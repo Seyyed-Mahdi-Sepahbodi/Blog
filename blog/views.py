ecf3347250ccb1bb86b2346f454fc7ce720518f4
Seyyed-Mahdi-Sepahbodi <seyyedmahdisepahbodi@gmail.com>
2022-03-23 08:27:14 -0400

    add index.html for home page.

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
+
+class HomePageView(View):
+    def get(self,request):
+        return render(request, 'blog/index.html')
--- myBlog/urls.py
+++ myBlog/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    # path('', include('blog.urls')),
+    path('', include('blog.urls')),
     path('panel/', include('panel.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
